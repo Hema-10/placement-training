043866b35548025c4481f9709bf851d6628c1f0a
HEMAPRIYA P <151361997+Hema-10@users.noreply.github.com>
2025-12-19 20:56:17 +0530

    Add Str-double.java to convert string to double

---
+++ 19-12-25/Str-double.java
@@ -0,0 +1,10 @@
+public class Main
+{
+    
+    public static void main(String args[])
+    {    
+        String str = "2033.12244";
+        double str1 = Double.parseDouble(str);
+        System.out.println(str1);
+    }
+}
