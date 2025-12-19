67a62528bf54f823d0fffba3e73414bfff727653
HEMAPRIYA P <151361997+Hema-10@users.noreply.github.com>
2025-12-19 08:10:49 +0530

    Add octal to decimal conversion example

---
+++ 20-12-25/oct-dec.java
@@ -0,0 +1,12 @@
+public class Geeks
+{
+    public static void main(String args[])
+    {
+        String oct = "157";
+        int num = Integer.parseInt(oct, 8);
+
+        System.out.println(
+            "Decimal equivalent of Octal value of 157 is: "
+            + num);
+    }
+}
