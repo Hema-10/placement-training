f525b433af8b2851e55ce4f885520154f79fe491
HEMAPRIYA P <151361997+Hema-10@users.noreply.github.com>
2025-12-19 20:54:03 +0530

    Add Bool-str.java to demonstrate Boolean toString

---
+++ 19-12-25/Bool-str.java
@@ -0,0 +1,14 @@
+class Geeks
+{ 
+	public static void main(String[] args) 
+	{ 
+		// boolean type value 
+		boolean value = false; 
+
+		// static toString() method of Boolean class 
+		String output = Boolean.toString(value); 
+
+		// printing the value 
+		System.out.println(output); 
+	} 
+}
